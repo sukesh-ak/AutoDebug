17f608676e5376c5846aebb60c393f7a7aa6a895
Sukesh <sukesh@live.com>
2020-06-23 00:08:00 +0530

    First commit

---
+++ AutoDebug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
